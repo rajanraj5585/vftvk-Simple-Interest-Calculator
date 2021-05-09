8bb78c674333b235be318d4efe3c679a3bbea559
rajanraj5585 <81469603+rajanraj5585@users.noreply.github.com>
2021-05-09 15:44:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function compute()
+ function calculateTip()
 {
-    p = document.getElementById("principal").value;
+     var billamt = document.getElementById("billamt").value;
+     var serviceQual = document.getElementById("serviceQual").value;
+     var intrate = document.getElementById("intrate").value;
     
+    
+    if(billamt==="" || serviceQual==0)
+        {
+            alert("please enter values");
+            reture;
+        }
+    
+    else
+    {
+        document.getElementById("each").style.display="block"; 
+    }
+    var total=(billamt*serviceQual*intrate)/100;
+    //total=Math.round(total);
+     document.getElementById("totalTip").style.display="block";
+     document.getElementById("tip").innerHTML=total;
 }
-        
+
+document.getElementById("totalTip").style.display="none";
+document.getElementById("totalTip").style.display="none";
+
+document.getElementById("calculate").onclick=function()
+{
+    calculateTip();
+}
